a40b5c1610e6ef48187625360f0181b40080f7e0
soomy1997 <57267548+soomy1997@users.noreply.github.com>
2023-11-08 06:55:04 +0300

    commit final changes

--- app/(dashboard)/[storeId]/(routes)/checkout/route.tsx
+++ app/(dashboard)/[storeId]/(routes)/checkout/route.tsx
@@ -1,4 +1,3 @@
-import { Stripe } from "stripe";
 import prismadb from "@/lib/prismadb";
 import { NextResponse } from "next/server";
 import axios from "axios";
@@ -8,7 +7,6 @@ const corsHeaders = {
   "Access—Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
   "Access—Control-Allow-Headers": "Content-Type, Authorization",
 };
-
 
 export async function OPTIONS() {
   return NextResponse.json({}, { headers: corsHeaders });
@@ -36,21 +34,6 @@ export async function POST(
       return total + product.price.toNumber(); // Assuming price is stored as a number in the database
     }, 0) * 100; // Moyasar's amount is in halalas (cents)
 
-  //   const line_items: Stripe.Checkout.SessionCreateParams.LineItem[] = [];
-
-  //   products.forEach((product) => {
-  //     line_items.push({
-  //       quantity: 1,
-  //       price_data: {
-  //         currency: "SAR",
-  //         product_data: {
-  //           name: product.name,
-  //         },
-  //         unit_amount: product.price.toNumber() * 100,
-  //       },
-  //     });
-  //   });
-
   const order = await prismadb.order.create({
     data: {
       storeId: params.storeId,
@@ -72,7 +55,6 @@ export async function POST(
       "https://api.moyasar.com/v1/payments",
       {
         amount: amount,
-        mode: "payment",
         currency: "SAR",
         description: "Order payment",
         callback_url: `${process.env.FRONTEND_STORE_URL}/payment-callback?orderId=${order.id}`,
@@ -82,8 +64,6 @@ export async function POST(
         credit_card: {
           save_card: "true",
         },
-        success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-        cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
 
         // You can add more fields as required by your application logic
       },
@@ -114,25 +94,4 @@ export async function POST(
       }
     );
   }
-
-  //   const session = await stripe.checkout.sessions.create({
-  //     line_items,
-  //     mode: "payment",
-  //     billing_address_collection: "required",
-  //     phone_number_collection: {
-  //       enabled: true,
-  //     },
-  //     success_url: `${process.env.FRONTEND_STORE_URL}/cart?success=1`,
-  //     cancel_url: `${process.env.FRONTEND_STORE_URL}/cart?canceled=1`,
-  //     metadata: {
-  //       orderlId: order.id,
-  //     },
-  //   });
-
-  //   return NextResponse.json(
-  //     { url: session.url },
-  //     {
-  //       headers: corsHeaders,
-  //     }
-  //   );
 }
